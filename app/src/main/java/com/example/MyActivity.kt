2ef75fac33510d24d9d2377a066eb8b3afab315c
Ataul Munim <hello@ataulm.com>
2019-05-14 12:04:52 +0100

    Initial commit

---
+++ app/src/main/java/com/example/MyActivity.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my)
+    }
+}
