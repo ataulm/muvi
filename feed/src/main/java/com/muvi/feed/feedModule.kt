5f1cdfa42f11fa0aab1bc903a91d6b780eab4a5b
Ataul Munim <hello@ataulm.com>
2019-07-11 23:56:37 +0100

    Add FeedDatabase to feed_data

--- feed/src/main/java/com/muvi/feed/feedModule.kt
+++ feed/src/main/java/com/muvi/feed/feedModule.kt
@@ -3,6 +3,7 @@ package com.muvi.feed
 import com.muvi.feed_data.AndroidFilmRepository
 import com.muvi.feed_domain.FilmRepository
 import com.muvi.feed_domain.GetFilmsUseCase
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -11,6 +12,7 @@ val feedModule = module(override = true) {
 
     factory<FilmRepository> {
         AndroidFilmRepository.create(
+                androidContext(),
                 BuildConfig.API_KEY,
                 BuildConfig.API_SECRET,
                 enableHttpLogging = BuildConfig.DEBUG
