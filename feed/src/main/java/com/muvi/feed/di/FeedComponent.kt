10f449a36601553743ab87102a75f9f07a8f9792
Ataul Munim <hello@ataulm.com>
2019-07-28 22:54:33 +0100

    Fix broken feed_remote
    - it was using build config but it's not android anymore!
    - provide these values via feed module with dagger qualifiers so we don't mix them up

--- feed/src/main/java/com/muvi/feed/di/FeedComponent.kt
+++ feed/src/main/java/com/muvi/feed/di/FeedComponent.kt
@@ -2,12 +2,16 @@ package com.muvi.feed.di
 
 import androidx.lifecycle.ViewModelProviders
 import com.muvi.core.di.CoreComponent
+import com.muvi.feed.BuildConfig
 import com.muvi.feed.FeedFragment
 import com.muvi.feed.FeedViewModel
 import com.muvi.feed.FeedViewModelFactory
 import com.muvi.feed_cache.di.FeedCacheModule
 import com.muvi.feed_data.di.FeedDataModule
+import com.muvi.feed_remote.di.DebugMode
 import com.muvi.feed_remote.di.FeedRemoteModule
+import com.muvi.feed_remote.di.LetterboxdApiKey
+import com.muvi.feed_remote.di.LetterboxdApiSecret
 import dagger.BindsInstance
 import dagger.Component
 import dagger.Module
@@ -37,6 +41,7 @@ internal interface FeedComponent {
 
         @BindsInstance
         fun fragment(fragment: FeedFragment): Builder
+
         fun coreComponent(module: CoreComponent): Builder
         fun build(): FeedComponent
     }
@@ -49,4 +54,19 @@ internal object FeedModule {
     @Provides
     fun viewModel(feedFragment: FeedFragment, viewModelFactory: FeedViewModelFactory) =
             ViewModelProviders.of(feedFragment, viewModelFactory).get(FeedViewModel::class.java)
+
+    @JvmStatic
+    @LetterboxdApiKey
+    @Provides
+    fun letterboxdApiKey() = BuildConfig.API_KEY
+
+    @JvmStatic
+    @LetterboxdApiSecret
+    @Provides
+    fun letterboxdApiSecret() = BuildConfig.API_SECRET
+
+    @JvmStatic
+    @DebugMode
+    @Provides
+    fun debugMode() = BuildConfig.DEBUG
 }
--- feed_remote/src/main/java/com/muvi/feed_remote/di/FeedRemoteModule.kt
+++ feed_remote/src/main/java/com/muvi/feed_remote/di/FeedRemoteModule.kt
@@ -1,6 +1,5 @@
 package com.muvi.feed_remote.di
 
-import com.muvi.feed_data.BuildConfig
 import com.muvi.feed_data.FeedRemote
 import com.muvi.feed_remote.LetterboxdFeedRemote
 import com.muvi.remote.Clock
@@ -8,13 +7,18 @@ import com.muvi.remote.LetterboxdApi
 import com.muvi.remote.LetterboxdApiFactory
 import dagger.Module
 import dagger.Provides
+import javax.inject.Qualifier
 
 @Module
 object FeedRemoteModule {
 
     @JvmStatic
     @Provides
-    fun feedRemote(): FeedRemote = LetterboxdFeedRemote(letterboxdApi())
+    fun feedRemote(
+            @LetterboxdApiKey apiKey: String,
+            @LetterboxdApiSecret apiSecret: String,
+            @DebugMode debugMode: Boolean
+    ): FeedRemote = LetterboxdFeedRemote(letterboxdApi(apiKey, apiSecret, debugMode))
 
     private fun clock(): Clock = object : Clock {
         override fun currentTimeMillis(): Long = System.currentTimeMillis()
@@ -22,10 +26,22 @@ object FeedRemoteModule {
 
     // TODO: we could break this up better - e.g. share OkHttpClient instance across app
     // that would give us two good scopes to showcase - per feature (e.g. RoomFeedDB) and per app
-    private fun letterboxdApi(): LetterboxdApi = LetterboxdApiFactory(
-            BuildConfig.API_KEY,
-            BuildConfig.API_SECRET,
+    private fun letterboxdApi(apiKey: String, apiSecret: String, debugMode: Boolean): LetterboxdApi = LetterboxdApiFactory(
+            apiKey,
+            apiSecret,
             clock(),
-            BuildConfig.DEBUG
+            debugMode
     ).remoteLetterboxdApi()
 }
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class LetterboxdApiKey
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class LetterboxdApiSecret
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DebugMode
