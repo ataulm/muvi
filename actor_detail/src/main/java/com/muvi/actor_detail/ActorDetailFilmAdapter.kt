5494b66cf9d57a4727aaddc5bc8a3647b09e725b
Ataul Munim <hello@ataulm.com>
2019-08-14 12:36:51 +0100

    Add R file for design_library

--- actor_detail/src/main/java/com/muvi/actor_detail/ActorDetailFilmAdapter.kt
+++ actor_detail/src/main/java/com/muvi/actor_detail/ActorDetailFilmAdapter.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.muvi.design_library.FilmSummaryView
+import com.muvi.design_library.R
 
 internal class ActorDetailFilmAdapter : ListAdapter<UiModel.Film, ActorDetailFilmAdapter.ViewHolder>(Differ) {
 
