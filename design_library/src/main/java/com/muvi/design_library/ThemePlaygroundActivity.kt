deb705f47c88f11361378f212ac58f3c424639fc
Ataul Munim <hello@ataulm.com>
2019-10-03 13:10:49 +0100

    Replace theme playground with third party version

--- design_library/src/main/java/com/muvi/design_library/ThemePlaygroundActivity.kt
+++ design_library/src/main/java/com/muvi/design_library/ThemePlaygroundActivity.kt
@@ -1,5 +1,14 @@
 package com.muvi.design_library
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ataulm.mdcshowcase.startMdcShowcase
 
-class ThemePlaygroundActivity : AppCompatActivity(R.layout.activity_theme_playground)
+class ThemePlaygroundActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startMdcShowcase(R.style.Theme_Muvi)
+        finish()
+    }
+}
